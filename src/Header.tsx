f28542f2026b61dfde2d271bdf4cc7186b3042fd
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-12-08 14:18:52 -0500

    header design finished
    TODO:
    add link functionality

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,14 @@
 import './Header.css'
+import Headroom from 'react-headroom'
 
 function Header() {
 
   return (
+
+    <div>
+    {/* headroom component makes navbar disappear and reappear on scroll */}
+    <div className="part1">
+    <Headroom>
     <div className="navbar">
         <h1 className="logo"> The Agency </h1>
         <div className="right-navbar">
@@ -10,6 +16,14 @@ function Header() {
         <h3 className="navbar-entry"> Learn More </h3>
         </div>
     </div>
+    </Headroom>
+    </div>
+    <div className="part2">
+        <h1> Part 2 </h1>
+    </div>
+    
+    </div>
+
   )
 }
 
