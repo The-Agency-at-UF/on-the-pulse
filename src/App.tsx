ed525cd334ca56ff04cebc02fc4a6fc9f2dbc01e
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-12-08 13:41:44 -0500

    added custom fonts and basic static navbar
    -added gentona (for about us and learn more)
    -added perandory (for agency logo)
    
    TODO:
    clicking functionality
    navbar animation | disappear on scroll
    media queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Header from './Header.tsx'
 
 function App() {
 
   return (
-    <></>
+    <Header/>
   )
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header() {
+
+  return (
+    <div className="navbar">
+        <h1 className="logo"> The Agency </h1>
+        <div className="right-navbar">
+        <h3 className="navbar-entry"> About Us </h3>
+        <h3 className="navbar-entry"> Learn More </h3>
+        </div>
+    </div>
+  )
+}
+
+export default Header
