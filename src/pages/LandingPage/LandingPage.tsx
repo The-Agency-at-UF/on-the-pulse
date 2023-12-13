b963cb39e80136f71525ccfdd517629c7347f2a1
John Miclat <jmiclat@ufl.edu>
2023-12-12 19:01:22 -0500

    add hover animation

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -7,7 +7,6 @@ import blob4 from '../../../public/images/blob4.png';
 import logo from '../../../public/images/landing-page-logo.png';
 
 function LandingPage() {
-  const logoAnimation = useAnimation();
 
   function getRandomAnimation() {
     // random values for x, y, scale, and rotate
@@ -41,40 +40,60 @@ function LandingPage() {
   const blob3Animation = getRandomAnimation();
   const blob4Animation = getRandomAnimation();
 
+
   // logo animation
+  const logoAnimation = useAnimation();
+
+  const startPulseAnimation = () => {
+    logoAnimation.start({
+      scale: [1, 1.035, 1],
+      transition: {
+        scale: {
+          duration: 4,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "easeInOut"
+        }
+      }
+    });
+  };
+
+  // useEffect to start the animation initially
   useEffect(() => {
-    // first float in the logo
     logoAnimation.start({
       y: 0,
       opacity: 1,
-      transition: { duration: 2, ease: "easeOut" },
+      transition: { duration: 1, ease: "easeOut" },
     })
-    .then(() => {
-      // after float in, start the pulse
-      logoAnimation.start({
-        scale: [1, 1.03, 1],
-        transition: {
-          scale: {
-            duration: 5,
-            repeat: Infinity,
-            repeatType: "reverse",
-            ease: "easeInOut"
-          }
-        }
-      });
-    });
+    .then(startPulseAnimation);
   }, [logoAnimation]);
+
+  // hover animation
+  const logoHoverAnimation = {
+    scale: 1.1, // slightly larger on hover
+    rotateY: [0, 10, -10, 10, 0], // rocking motion degrees
+    transition: {
+      scale: { duration: 0.3, ease: "easeInOut" },
+      rotateY: {
+        duration: 0.6, // duration of one complete rock back and forth
+        ease: "easeInOut",
+        yoyo: Infinity // repeat the animation indefinitely
+      }
+    }
+  };  
 
   return (
     <div className="relative h-screen flex justify-center items-center">
       {/* Logo in the center */}
       <div className="absolute z-10 w-2/3 sm:w-1/2 md:w-3/5 lg:w-1/2">
         <motion.img 
-            src={logo}
-            alt="Landing Page Logo"
-            initial={{ y: -50, opacity: 0 }}
-            animate={logoAnimation}
-            className="max-w-full h-auto" // maintain aspect ratio and full width
+          src={logo}
+          alt="Landing Page Logo"
+          initial={{ y: -50, opacity: 0 }}
+          animate={logoAnimation}
+          whileHover={logoHoverAnimation}
+          onHoverEnd={startPulseAnimation} // Restart pulse animation after hover
+          className="max-w-full h-auto"
         />
       </div>
 
