37bdcd106b8ce4a421eecd6b9c5cf93cd50d3a6b
John Miclat <jmiclat@ufl.edu>
2023-12-15 16:16:33 -0500

    Lovely blob and logo hover animations

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -4,6 +4,7 @@ import { blob1, blob2, blob3, blob4, logo } from '../../assets/images';
 
 function LandingPage() {
 
+  // randomize blob animations
   function getRandomAnimation() {
     const randomX = [0, Math.random() * 30 - 15, Math.random() * 30 - 15, 0];
     const randomY = [0, Math.random() * 30 - 15, Math.random() * 30 - 15, 0];
@@ -29,11 +30,37 @@ function LandingPage() {
   }  
   
   // define animation for the blobs
-  const blob1Animation = getRandomAnimation();
-  const blob2Animation = getRandomAnimation();
-  const blob3Animation = getRandomAnimation();
-  const blob4Animation = getRandomAnimation();
+  const blob1Controls = useAnimation();
+  const blob2Controls = useAnimation();
+  const blob3Controls = useAnimation();
+  const blob4Controls = useAnimation();
 
+  // initialize random animations on component mount
+  useEffect(() => {
+    blob1Controls.start(getRandomAnimation());
+    blob2Controls.start(getRandomAnimation());
+    blob3Controls.start(getRandomAnimation());
+    blob4Controls.start(getRandomAnimation());
+  }, [blob1Controls, blob2Controls, blob3Controls, blob4Controls]);
+
+  const onHoverStart = (controls) => {
+    controls.start({
+      scale: [1.05, 1.1, 1.05, 1.15, 1.05, 1.1, 1.05], // keyframes for pulsing effect
+      transition: {
+        duration: 0.7,
+        ease: "easeInOut",
+        loop: Infinity, // loop the animation indefinitely
+      }
+    });
+  };
+  
+  const onHoverEnd = (controls) => {
+    controls.start({ 
+      scale: 1,
+      transition: { duration: 0.3, ease: "easeInOut" } 
+    });
+  };
+  
 
   // logo animation
   const logoAnimation = useAnimation();
@@ -76,19 +103,28 @@ function LandingPage() {
     }
   };  
 
+  // smooth handling of hover end
+  const handleLogoHoverEnd = () => {
+    logoAnimation.start({
+      scale: 1, // Reset scale to normal
+      transition: { duration: 0.3, ease: "easeInOut" }
+    }).then(startPulseAnimation); // Then restart the pulse animation
+  };
+  
+
   return (
     <div className="relative h-screen flex justify-center items-center">
       {/* Logo in the center */}
       <div className="absolute z-10 w-2/3 sm:w-3/4 md:w-3/5 lg:w-1/2">
-        <motion.img 
-          src={logo}
-          alt="Landing Page Logo"
-          initial={{ y: -50, opacity: 0 }}
-          animate={logoAnimation}
-          whileHover={logoHoverAnimation}
-          onHoverEnd={startPulseAnimation} // Restart pulse animation after hover
-          className="max-w-full h-auto"
-        />
+      <motion.img 
+        src={logo}
+        alt="Landing Page Logo"
+        initial={{ y: -50, opacity: 0 }}
+        animate={logoAnimation}
+        whileHover={logoHoverAnimation}
+        onHoverEnd={handleLogoHoverEnd}
+        className="max-w-full h-auto"
+      />
       </div>
 
 
@@ -98,28 +134,36 @@ function LandingPage() {
           alt="Blob 1"
           className="absolute top-12 left-[10rem] w-1/4 sm:left-20 sm:w-[12rem] 
                     md:left-[6rem] md:w-[12rem] lg:w-[14rem]"
-          animate={blob1Animation}
+          animate={blob1Controls}
+          onHoverStart={() => onHoverStart(blob1Controls)}
+          onHoverEnd={() => onHoverEnd(blob1Controls)}
       />
       <motion.img 
           src={blob2}
           alt="Blob 2"
           className="absolute bottom-0 left-0 lg:-ml-26 w-1/3 sm:w-[24rem] sm:-ml-24
                      md:-ml-28 md:w-[28rem] lg:w-[30rem]"
-          animate={blob2Animation}
+          animate={blob2Controls}
+          onHoverStart={() => onHoverStart(blob2Controls)}
+          onHoverEnd={() => onHoverEnd(blob2Controls)}
       />
       <motion.img 
           src={blob3}
           alt="Blob 3"
           className="absolute bottom-[4rem] right-[6rem] w-1/5 sm:right-0 sm:w-1/3 
                       md:right-0 md:w-[16rem] lg:right-[5rem] lg:w-[18rem]"
-          animate={blob3Animation}
-      />
+          animate={blob3Controls}
+          onHoverStart={() => onHoverStart(blob3Controls)}
+          onHoverEnd={() => onHoverEnd(blob3Controls)}
+/>
       <motion.img 
           src={blob4}
           alt="Blob 4"
           className="absolute top-0 right-0 -mr-32 w-1/4 sm:right-0 sm:bottom-0 sm:w-1/3 
                       md:right-10 md:w-[16rem] lg:right-5 lg:w-1/5"
-          animate={blob4Animation}
+          animate={blob4Controls}
+          onHoverStart={() => onHoverStart(blob4Controls)}
+          onHoverEnd={() => onHoverEnd(blob4Controls)}
       />
     </div>
   )
