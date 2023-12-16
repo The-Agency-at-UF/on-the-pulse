d14a9b1a72a48c0cbf63822d3b867fc83ee75fe3
John Miclat <jmiclat@ufl.edu>
2023-12-16 18:27:09 -0500

    Add global fonts and global colors, move fonts into preferred file location

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './Header';
-import LandingPage from './pages/LandingPage/LandingPage';
+import LandingPage from './pages/LandingPage';
+import LearnMore from './pages/LearnMore';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Header />
           <Routes>
               <Route path="/" element={<LandingPage />} />
+              <Route path="/learn-more" element={<LearnMore />} />
           </Routes>
       </Router>
   );
