fd8cdbcf869120a3ae8a3879de7b6a6a937f01ed
John Miclat <jmiclat@ufl.edu>
2023-12-16 19:02:09 -0500

    Finish checkbox styling

--- src/pages/LearnMore.tsx
+++ src/pages/LearnMore.tsx
@@ -3,9 +3,24 @@ import React from 'react'
 function LearnMore() {
   return (
     <>
-      <h2>
-        Learn More
-      </h2>
+    <div>
+        <h2>Learn More</h2>   
+
+        <form action="" class="flex justify-center items-center min-h-screen">
+            <div>
+                <label class="form-control flex items-center gap-2 font-bold text-xl">
+                <input type="checkbox" name="checkbox" class="checkbox" />
+                blah1
+                </label>
+
+                <label class="form-control flex items-center gap-2 font-bold text-xl mt-4">
+                <input type="checkbox" name="checkbox-checked" class="checkbox" />
+                blah 2
+                </label>
+            </div>
+        </form>
+
+    </div>
     </>
   )
 }
