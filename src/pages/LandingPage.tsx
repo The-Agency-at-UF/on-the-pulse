0ccce7c89fd365df8a4c67d891dcedb5f2dc6077
John Miclat <jmiclat@ufl.edu>
2023-12-16 20:06:00 -0500

    Modularize blob animations

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,33 +1,11 @@
 import { motion, useAnimation } from 'framer-motion';
 import React, { useEffect } from 'react';
 import { blob1, blob2, blob3, blob4, logo } from '../assets/images/landing-page';
+import { getRandomAnimation, onHoverEnd, onHoverStart } from '../utils/animations';
 
 function LandingPage() {
 
-  // randomize blob animations
-  function getRandomAnimation() {
-    const randomX = [0, Math.random() * 30 - 15, Math.random() * 30 - 15, 0];
-    const randomY = [0, Math.random() * 30 - 15, Math.random() * 30 - 15, 0];
-    const randomScale = [1, 1 + Math.random() * 0.15, 1 - Math.random() * 0.15, 1];
-    const randomRotate = [0, Math.random() * 30 - 15, Math.random() * 30 - 15, 0];
-  
-    const duration = 12 + Math.random() * 8;
-    const delay = Math.random() * 3;
-  
-    return {
-      x: randomX,
-      y: randomY,
-      scale: randomScale,
-      rotate: randomRotate,
-      transition: {
-        x: { repeat: Infinity, repeatType: "reverse", duration, delay },
-        y: { repeat: Infinity, repeatType: "reverse", duration, delay },
-        scale: { repeat: Infinity, repeatType: "reverse", duration, delay },
-        rotate: { repeat: Infinity, repeatType: "reverse", duration, delay },
-        ease: "easeInOut"
-      }
-    };
-  }  
+
   
   // define animation for the blobs
   const blob1Controls = useAnimation();
@@ -43,24 +21,7 @@ function LandingPage() {
     blob4Controls.start(getRandomAnimation());
   }, [blob1Controls, blob2Controls, blob3Controls, blob4Controls]);
 
-  const onHoverStart = (controls) => {
-    controls.start({
-      scale: [1.05, 1.1, 1.05, 1.15, 1.05, 1.1, 1.05], // keyframes for pulsing effect
-      transition: {
-        duration: 0.7,
-        ease: "easeInOut",
-        loop: Infinity, // loop the animation indefinitely
-      }
-    });
-  };
-  
-  const onHoverEnd = (controls) => {
-    controls.start({ 
-      scale: 1,
-      transition: { duration: 0.3, ease: "easeInOut" } 
-    });
-  };
-  
+
 
   // logo animation
   const logoAnimation = useAnimation();
