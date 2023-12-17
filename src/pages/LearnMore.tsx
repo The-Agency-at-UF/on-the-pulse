1500e6bb78794058821f4b131d6b8da814d1926b
John Miclat <jmiclat@ufl.edu>
2023-12-16 19:34:41 -0500

    Add and format Learn More page content

--- src/pages/LearnMore.tsx
+++ src/pages/LearnMore.tsx
@@ -3,26 +3,53 @@ import React from 'react'
 function LearnMore() {
   return (
     <>
-    <div>
-        <h2>Learn More</h2>   
+      <div class="flex flex-col items-center justify-center min-h-screen">
+        
+        <h2>Learn More</h2>
+        
+        <div class="w-full max-w-2xl">
 
-        <form action="" class="flex justify-center items-center min-h-screen">
-            <div>
-                <label class="form-control flex items-center gap-2 font-bold text-xl">
-                <input type="checkbox" name="checkbox" class="checkbox" />
-                blah1
-                </label>
+          <h3>Describe Your Inquiry</h3>
+          
+          <form action="" class="space-y-4">
+            <label class="form-control flex items-center gap-2 text-xl">
+              <input type="checkbox" name="checkbox" class="checkbox" />
+              I'm interested in a cusom workshop on an On The Pulse topic.
+            </label>
+            <label class="form-control flex items-center gap-2 text-xl">
+              <input type="checkbox" name="checkbox-checked" class="checkbox" />
+              I'm interested in working with The Agency.
+            </label>
 
-                <label class="form-control flex items-center gap-2 font-bold text-xl mt-4">
-                <input type="checkbox" name="checkbox-checked" class="checkbox" />
-                blah 2
-                </label>
-            </div>
-        </form>
+            <label class="form-control flex items-center gap-2 text-xl">
+              <input type="checkbox" name="checkbox-checked" class="checkbox" />
+              Other
+            </label>
+            <textarea name="other" class="w-full p-2 h-36" 
+            placeholder="Specify your request."/>
 
-    </div>
+            <h3>Contact Information</h3>
+            
+            <h4 class="text-lg">First Name</h4>
+            <input type="text" name="firstName" class="w-full p-2" />
+
+            <h4 class="text-lg">Last Name</h4>
+            <input type="text" name="lastName" class="w-full p-2" />
+
+            <h4 class="text-lg">Company</h4>
+            <input type="text" name="company" class="w-full p-2" />
+
+            <h4 class="text-lg">Email Address</h4>
+            <input type="text" name="email" class="w-full p-2" />
+
+
+
+          </form>
+        </div>
+      </div>
     </>
   )
 }
+
 
 export default LearnMore
