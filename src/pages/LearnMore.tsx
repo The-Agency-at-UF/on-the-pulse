68571968e0e775207882eafa0bc9d3315d6d7668
John Miclat <jmiclat@ufl.edu>
2023-12-16 19:42:17 -0500

    Add responsitivity

--- src/pages/LearnMore.tsx
+++ src/pages/LearnMore.tsx
@@ -3,44 +3,46 @@ import React from 'react'
 function LearnMore() {
   return (
     <>
-      <div class="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center min-h-screen px-4">
         
-        <h2>Learn More</h2>
+        <h2 className="text-5xl md:text-6xl font-bold text-center mb-6">
+            Learn More
+        </h2>
         
-        <div class="w-full max-w-2xl">
+        <div className="w-full max-w-2xl">
 
-          <h3 class='pb-4'>Describe Your Inquiry</h3>
+          <h3 className="text-xl md:text-2xl font-semibold pb-4">Describe Your Inquiry</h3>
           
-          <form action="" class="space-y-4">
-            <label class="form-control flex items-center gap-2 text-xl">
-              <input type="checkbox" name="checkbox" class="checkbox" />
-              I'm interested in a cusom workshop on an On The Pulse topic.
+          <form action="" className="space-y-4">
+            <label className="form-control flex items-center gap-2 text-lg md:text-xl">
+              <input type="checkbox" name="checkbox" className="checkbox" />
+              I'm interested in a custom workshop on an On The Pulse topic.
             </label>
-            <label class="form-control flex items-center gap-2 text-xl">
-              <input type="checkbox" name="checkbox-checked" class="checkbox" />
+            <label className="form-control flex items-center gap-2 text-lg md:text-xl">
+              <input type="checkbox" name="checkbox-checked" className="checkbox" />
               I'm interested in working with The Agency.
             </label>
 
-            <label class="form-control flex items-center gap-2 text-xl">
-              <input type="checkbox" name="checkbox-checked" class="checkbox" />
+            <label className="form-control flex items-center gap-2 text-lg md:text-xl">
+              <input type="checkbox" name="checkbox-checked" className="checkbox" />
               Other
             </label>
-            <textarea name="other" class="w-full p-2 h-36" 
+            <textarea name="other" className="w-full p-2 h-36" 
             placeholder="Specify your request."/>
 
-            <h3>Contact Information</h3>
+            <h3 className="text-xl md:text-2xl font-semibold mt-6">Contact Information</h3>
             
-            <h4 class="text-lg">First Name</h4>
-            <input type="text" name="firstName" class="w-full p-2" />
+            <h4 className="text-sm md:text-lg mt-4">First Name</h4>
+            <input type="text" name="firstName" className="w-full p-2" />
 
-            <h4 class="text-lg">Last Name</h4>
-            <input type="text" name="lastName" class="w-full p-2" />
+            <h4 className="text-sm md:text-lg mt-4">Last Name</h4>
+            <input type="text" name="lastName" className="w-full p-2" />
 
-            <h4 class="text-lg">Company</h4>
-            <input type="text" name="company" class="w-full p-2" />
+            <h4 className="text-sm md:text-lg mt-4">Company</h4>
+            <input type="text" name="company" className="w-full p-2" />
 
-            <h4 class="text-lg">Email Address</h4>
-            <input type="text" name="email" class="w-full p-2" />
+            <h4 className="text-sm md:text-lg mt-4">Email Address</h4>
+            <input type="text" name="email" className="w-full p-2" />
 
           </form>
         </div>
@@ -48,6 +50,5 @@ function LearnMore() {
     </>
   )
 }
-
 
 export default LearnMore
