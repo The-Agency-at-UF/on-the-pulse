bc8f3d8de5548950b735fa4f791d1f9f399374ab
John Miclat <jmiclat@ufl.edu>
2023-12-17 19:57:48 -0500

    Add responsitivity

--- src/pages/LearnMore.tsx
+++ src/pages/LearnMore.tsx
@@ -6,78 +6,91 @@ import { getRandomAnimation, onHoverEnd, onHoverStart } from '../utils/animation
 
 function LearnMore() {
 
-    // blob animations
-    const topLeftControls = useAnimation();
-    const topRightControls = useAnimation();
-    const bottomLeftControls = useAnimation();
-    const bottomRightControls = useAnimation();
+  // blob animations
+  const topLeftControls = useAnimation();
+  const topRightControls = useAnimation();
+  const bottomLeftControls = useAnimation();
+  const bottomRightControls = useAnimation();
 
-    // submitBlob animation
-    const submitBlobControls = useAnimation();
-  
-    useEffect(() => {
-      topLeftControls.start(getRandomAnimation());
-      topRightControls.start(getRandomAnimation());
-      bottomLeftControls.start(getRandomAnimation());
-      bottomRightControls.start(getRandomAnimation());
+  // submitBlob animation
+  const submitBlobControls = useAnimation();
 
-      //submitBlob
-      submitBlobControls.start(getRandomAnimation());
-    }, [topLeftControls, topRightControls, 
-      bottomLeftControls, bottomRightControls,
-      submitBlobControls]);
-  
+  useEffect(() => {
+    topLeftControls.start(getRandomAnimation());
+    topRightControls.start(getRandomAnimation());
+    bottomLeftControls.start(getRandomAnimation());
+    bottomRightControls.start(getRandomAnimation());
+
+    //submitBlob
+    submitBlobControls.start(getRandomAnimation());
+  }, [topLeftControls, topRightControls, 
+    bottomLeftControls, bottomRightControls,
+    submitBlobControls]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('submitted');
+  }
+
+
   return (
     <>
-    <div className="">
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className='blob-container'>
         {/* Blob Elements */}
         <motion.img 
           src={blobTopLeft}
           alt="Blob Top Left"
-          className="absolute top-0 left-0"
+          className="blob absolute top-8 left-[-3rem] w-1/3
+          sm:top-6 sm:left-[-6rem] sm:w-[12rem]"
           animate={topLeftControls}
           onHoverStart={() => onHoverStart(topLeftControls)}
           onHoverEnd={() => onHoverEnd(topLeftControls)}
           draggable="false"
         />
         <motion.img 
-          src={blobTopRight}
-          alt="Blob Top Right"
-          className="absolute top-0 right-0"
-          animate={topRightControls}
-          onHoverStart={() => onHoverStart(topRightControls)}
-          onHoverEnd={() => onHoverEnd(topRightControls)}
-          draggable="false"
-        />
-        <motion.img 
           src={blobBottomLeft}
           alt="Blob Bottom Left"
-          className="absolute bottom-0 left-0"
+          className="absolute bottom-[-4rem] left-[-9rem] w-1/2
+          sm:bottom-[-7rem] sm:left-[-8rem] sm:w-[16rem]"
           animate={bottomLeftControls}
           onHoverStart={() => onHoverStart(bottomLeftControls)}
           onHoverEnd={() => onHoverEnd(bottomLeftControls)}
           draggable="false"
         />
         <motion.img 
+          src={blobTopRight}
+          alt="Blob Top Right"
+          className="absolute top-[6rem] right-0 w-[8rem]
+          sm:top-[5rem] sm:right-0 sm:w-[10rem]"
+          animate={topRightControls}
+          onHoverStart={() => onHoverStart(topRightControls)}
+          onHoverEnd={() => onHoverEnd(topRightControls)}
+          draggable="false"
+        />
+        <motion.img 
           src={blobBottomRight}
           alt="Blob Bottom Right"
-          className="absolute bottom-0 right-0"
+          className="absolute bottom-0 right-0 w-1/4
+          sm:bottom-[-2rem] sm:right-0 sm:w-[12rem]"
           animate={bottomRightControls}
           onHoverStart={() => onHoverStart(bottomRightControls)}
           onHoverEnd={() => onHoverEnd(bottomRightControls)}
           draggable="false"
         />
-    </div>
-      <div className="flex flex-col items-center justify-center min-h-screen px-4">
+      </div>
+
+      {/* Content */}
+      <div className="mt-6 z-10 w-full px-4">
         <h2 className="text-5xl md:text-6xl font-bold text-center mb-6">
             Learn More
         </h2>
         
-        <div className="w-full max-w-2xl">
+        <div className="w-full mx-auto max-w-2xl">
 
           <h3 className="text-xl md:text-2xl font-semibold pb-4">Describe Your Inquiry</h3>
           
-          <form action="" className="space-y-4">
+          <form action="" className="space-y-4" onSubmit={handleSubmit}>
             <label className="form-control flex items-center gap-2 text-lg md:text-xl">
               <input type="checkbox" name="checkbox" className="checkbox" />
               I'm interested in a custom workshop on an On The Pulse topic.
@@ -107,8 +120,8 @@ function LearnMore() {
 
             <h4 className="text-sm md:text-lg mt-4">Email Address</h4>
             <input type="text" name="email" className="w-full p-2" />
-
-            <button type="submit" 
+            
+            <button type="submit"
             className='flex justify-center items-center w-full'>
               <motion.img 
                 src={submitBlob}
@@ -124,6 +137,8 @@ function LearnMore() {
           </form>
         </div>
       </div>
+      </div>
+
     </>
   )
 }
