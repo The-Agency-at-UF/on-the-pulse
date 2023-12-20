25b7286d047e1fcaa21785dcb8eb8208dd78e0c0
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-12-20 12:51:01 -0500

    migrated navbar and updates to john's version of the website

---
+++ src/Hamburger.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import './Hamburger.css'
+import icon from "./assets/images/hamburger-menu.svg"
+import {useState} from "react"
+import {Link} from "react-router-dom"
+
+
+const Hamburger: FC = () => {
+  const [hamburgerOpen, setHamburger] = useState<boolean>(false);
+
+  const toggleHamburger = () => {
+    setHamburger(!hamburgerOpen)
+  }
+
+  return (
+    <>
+      <div className="hamburger-menu" onClick={toggleHamburger}>
+      <img className="hamburger-icon" src={icon}/> 
+      </div>
+
+    <div className="hamburger-overlay">
+      <div className="hamburger-links"> 
+        <h3 className="navbar-entry hamburger-entry"> About Us </h3>
+        <h3 className="navbar-entry hamburger-entry"> Learn More </h3>
+      </div>
+    </div> 
+
+      <style jsx>{`
+        .hamburger-entry{
+          display: ${hamburgerOpen ? 'inherit' : 'none'};
+        }
+
+        .hamburger-overlay{
+          background-color: ${hamburgerOpen ? 'rgba(0,0,0,.80)' : 'inherit'};
+        }
+
+        html, body {
+            overflow-y: ${hamburgerOpen ? 'hidden' : 'inherit'};
+        }
+        
+        
+      `}</style>
+    </>
+  );
+};
+
+export default Hamburger;
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,22 +1,33 @@
 import './Header.css'
 import Headroom from 'react-headroom'
+import Hamburger from './Hamburger'
 
-function Header() {
 
+const Header: React.FC = () => {
+
+  
   return (
-
+    <>
     <div>
     {/* headroom component makes navbar disappear and reappear on scroll */}
+
     <Headroom>
+
     <div className="navbar">
-        <h1 className="logo"> The Agency </h1>
+        <h1 className="logo"> <a href="https://theagency.jou.ufl.edu/"> The Agency </a> </h1>
         <div className="right-navbar">
+        <div className="standard-right">
         <h3 className="navbar-entry"> About Us </h3>
         <h3 className="navbar-entry"> Learn More </h3>
+        </div>
+        <div className="hamburger"> 
+        <Hamburger/>
+        </div>
         </div>
     </div>
     </Headroom>
     </div>
+    </>
 
 
   )
