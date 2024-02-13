bdeb0443cab0efd4ebfd4fe6ae93ce069cd0d71b
tam-n-huynh <tamhuynh3861@gmail.com>
2024-02-13 10:12:06 -0500

    Added preview page and link for getting the starred posts to appear on the landing page.

--- src/assets/images/landing-page/index.js
+++ src/assets/images/landing-page/index.js
@@ -3,5 +3,10 @@ import blob2 from './blob2.png';
 import blob3 from './blob3.png';
 import blob4 from './blob4.png';
 import logo from './landing-page-logo.png';
+import LandingText from './LandingText.png';
+import LandingTextSVG from './LandingTextSVG.svg';
+import favblog1 from './fav-blog1.png';
+import favblog2 from './fav-blog2.png';
+import favblog3 from './fav-blog3.png';
 
-export { blob1, blob2, blob3, blob4, logo };
+export { blob1, blob2, blob3, blob4, logo, LandingText, LandingTextSVG, favblog1, favblog2, favblog3 };
